b936cf00da2c4f743aa84038ee44cf034c59068e
sup226 <rlaalswl1102@naver.com>
2024-10-17 11:52:27 +0900

    EntityManager를 이용한 CRUD (영속성컨텍스트의 특징 확인)

--- src/main/java/com/study/jpa/chap01/entity/Product.java
+++ src/main/java/com/study/jpa/chap01/entity/Product.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
+@Setter
 @Getter @ToString
 @EqualsAndHashCode(of = "id")
 @NoArgsConstructor
--- src/test/java/com/study/jpa/chap01/repository/ProductRepositoryTest.java
+++ src/test/java/com/study/jpa/chap01/repository/ProductRepositoryTest.java
@@ -1,7 +1,90 @@
 package com.study.jpa.chap01.repository;
 
+import com.study.jpa.chap01.entity.Product;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@Transactional
+@Rollback(false)
 class ProductRepositoryTest {
+
+    // EntityManager를 이용하는 방법
+    @Autowired
+    EntityManager em;
+
+    @Test
+    @DisplayName("상품을 데이터베이스에 저장한다.")
+    void saveTest() {
+        // given
+        Product p = Product.builder()
+                .name("신발")
+                .price(90000)
+                .category(Product.Category.FASHION)
+                .build();
+
+        // when
+        em.persist(p); // 영속화(영속컨테이트에 영속 상태가 됨) -> 매니저가 데이터베이스에 반영(insert)함
+
+        // then
+    }
+
+    @Test
+    @DisplayName("저장된 상품을 조회한다.")
+    void selectTest() {
+        // given
+        Long id = 1L;
+
+        // when
+        Product product = em.find(Product.class, id);
+
+        // then
+        Assertions.assertEquals("신발", product.getName());
+    }
+
+    @Test
+    @DisplayName("영속성 컨텍스트의 1차 캐시 사용")
+    void persistTest() {
+        // given
+        Product p = Product.builder()
+                .name("짜장면")
+                .price(8000)
+                .category(Product.Category.FOOD)
+                .build();
+
+        // when
+        em.persist(p);
+
+        Product findProd = em.find(Product.class, 2L);
+
+        // then
+        assertEquals(8000, findProd.getPrice());
+    }
+
+    @Test
+    @DisplayName("특정 상품의 가격을 수정한다.")
+    void updateTest() {
+        // given
+        Product shoes = em.find(Product.class, 1L);
+
+        // when
+        shoes.setPrice(50000);
+        shoes.setName("할인하는 신발");
+
+        em.persist(shoes);
+
+        int price = em.find(Product.class, 1L).getPrice();
+
+        // then
+        assertEquals(50000, price);
+    }
 
 }
