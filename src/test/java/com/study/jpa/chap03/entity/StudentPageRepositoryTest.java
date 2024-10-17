1f891cf14c3e954bb5bea67be4194e6ea2064d49
sup226 <rlaalswl1102@naver.com>
2024-10-17 17:18:45 +0900

    JPA Sorting

--- src/test/java/com/study/jpa/chap03/entity/StudentPageRepositoryTest.java
+++ src/test/java/com/study/jpa/chap03/entity/StudentPageRepositoryTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
@@ -44,7 +45,16 @@ class StudentPageRepositoryTest {
 
         // 페이지 정보 객체 생성 (Pageable)
         // 여기서는 페이지 번호가 zero-base임: 1페이지를 0으로 취급
-        Pageable pageInfo = PageRequest.of(PageNo -1, amount);
+        Pageable pageInfo = PageRequest.of(
+                PageNo -1,
+                amount,
+//                Sort.by("name").descending() // 정렬 기준은 필드명!!! 컬럼명 x
+                // 여러 조건으로 정렬
+                Sort.by(
+                        Sort.Order.desc("name"),
+                        Sort.Order.asc("city")
+                )
+        );
 
         // when
         Page<Student> students = repository.findAll(pageInfo);
