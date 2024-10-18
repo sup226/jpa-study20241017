e117d9c86e929dd94f9ed4f189792db47583f13a
sup226 <rlaalswl1102@naver.com>
2024-10-18 15:45:38 +0900

    Query DSL과 JPQL 비교

---
+++ src/main/java/com/study/jpa/chap05/entity/Idol.java
@@ -0,0 +1,39 @@
+package com.study.jpa.chap05.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tbl_idol")
+@Setter
+@Getter
+@ToString(exclude = "group")
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 접근 제한자 설정
+public class Idol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idol_id")
+    private Long id;
+    private String idolName;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    public Idol(String idolName, int age, Group group) {
+        this.idolName = idolName;
+        this.age = age;
+        if (group != null) {
+            changeGroup(group);
+        }
+    }
+
+    public void changeGroup(Group group) {
+        this.group = group;
+        group.getIdols().add(this);
+    }
+
+}
