c3cfcb63e7a49ce748fa3477879fbbee7ead1946
sup226 <rlaalswl1102@naver.com>
2024-10-18 11:30:43 +0900

    단방향 연관관계 확인, 양방향 연관관계 세팅, fetchType Eager와 Lazy의 차이, 양방향 관계에서 데이터 갱신이 반대편에 반영되지 않는 문제

--- src/main/java/com/study/jpa/chap04/entity/Employee.java
+++ src/main/java/com/study/jpa/chap04/entity/Employee.java
@@ -23,7 +23,9 @@ public class Employee {
     @Column(name = "emp_name", nullable = false)
     private String name; // 사원명
 
-    @ManyToOne
+    // EAGER: 해당 필드를 사용 하든 말든 항상 무조건 조인을 수행
+    // LAZY: 필요한 경우에만 데이터를 가져온다. -> 실무
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "dept_id") // FK 컬럼명 (연관 테이블의 컬럼명과 일치하게 작성)
     private Department department;
 
